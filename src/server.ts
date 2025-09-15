e1f3e5ab32bba5409383c3ff102098e620102128
MD SIFAT AHAMED~ <mdsifat978@gmail.com>
2025-09-15 13:01:08 +0600

    initial project setup done

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import app from './app'
-const mongoose = require('mongoose');
-
+import app from './app';
+import config from './app/config';
+import mongoose from 'mongoose';
 
 async function main() {
-    await mongoose.connect(process.env.DATABASE_URL);
+  try {
+    await mongoose.connect(config.database_url as string);
 
-    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-app.listen(process.env.PORT, () => {
-    console.log(`Example app listening on port ${process.env.PORT}`)
-})
-
-
-// mdsifat978_db_user
-// eOCwcu6Wp1ZhlpsW
+main();
