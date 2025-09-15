3ddf796b5cb7b7714555662f18ec594c43d4104c
MD SIFAT AHAMED~ <mdsifat978@gmail.com>
2025-09-15 13:40:56 +0600

    json start script add

--- dist/server.js
+++ dist/server.js
@@ -13,15 +13,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
-const mongoose = require('mongoose');
+const config_1 = __importDefault(require("./app/config"));
+const mongoose_1 = __importDefault(require("mongoose"));
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
-        yield mongoose.connect(process.env.DATABASE_URL);
-        // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+        try {
+            yield mongoose_1.default.connect(config_1.default.database_url);
+            app_1.default.listen(config_1.default.port, () => {
+                console.log(`Example app listening on port ${config_1.default.port}`);
+            });
+        }
+        catch (error) {
+            console.log(error);
+        }
     });
 }
-app_1.default.listen(process.env.PORT, () => {
-    console.log(`Example app listening on port ${process.env.PORT}`);
-});
-// mdsifat978_db_user
-// eOCwcu6Wp1ZhlpsW
+main();
